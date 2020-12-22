b6c397a09369fe36792e41ce82da400cf6c7c844
Benoit Tassin <benoit.tassin@live.fr>
2020-12-22 18:19:11 +0100

    Fix imports in ng-responsive-ui public api

--- projects/ng-responsive-ui/src/public-api.ts
+++ projects/ng-responsive-ui/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/ng-responsive-ui.service';
 export * from './lib/ng-responsive-ui.module';
+export * from './lib/responsive-container.directive';
+export * from './lib/responsive-grid.directive';
